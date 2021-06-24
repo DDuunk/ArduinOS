c9551d9d1425481e71e2d11c076da179796feaba
Dylan <dylan@protium.digital>
2021-06-24 16:15:26 +0200

    feat(stack): improve memory usage

--- src/stack.cpp
+++ src/stack.cpp
@@ -1,123 +1,123 @@
 #include "stack.h"
 
-//Stack::Stack() {
-////  peeked_ = false;
-////  pos_ = 0;
-////
-////  PushByte(66);
-////  Serial.println("Stack instance created");
-//}
-//
-//Stack::~Stack() {
-//}
-
-void Stack::PushByte(stack_t* stack, uint8_t elem) {
-  stack->stack[(stack->pos)++] = elem;
+Stack::Stack() {
+  Reset();
 }
 
-uint8_t Stack::PopByte(stack_t* stack) {
-  return stack->stack[--(stack->pos)];
+Stack::~Stack() {}
+
+void Stack::Reset() {
+  memset(stack_, '\0', sizeof(stack_));
+  pos_ = 0;
 }
 
-void Stack::PushChar(stack_t* stack, char elem) {
-  PushByte(stack, elem);
-  PushByte(stack, CHAR);
+void Stack::PushByte(uint8_t elem) {
+  stack_[pos_++] = elem;
 }
 
-char Stack::PopChar(stack_t* stack) {
-  PopByte(stack);
-  return PopByte(stack);
+uint8_t Stack::PopByte() {
+  return stack_[--pos_];
 }
 
-void Stack::PushInt(stack_t* stack, int elem) {
-  PushByte(stack, highByte(elem));
-  PushByte(stack, lowByte(elem));
-  PushByte(stack, INT);
+void Stack::PushChar(char elem) {
+  PushByte(elem);
+  PushByte(CHAR);
 }
 
-int Stack::PopInt(stack_t* stack) {
-  PopByte(stack);
-  uint8_t high_byte = PopByte(stack);
-  uint8_t low_byte = PopByte(stack);
+char Stack::PopChar() {
+  PopByte();
+  return PopByte();
+}
+
+void Stack::PushInt(int elem) {
+  PushByte(highByte(elem));
+  PushByte(lowByte(elem));
+  PushByte(INT);
+}
+
+int Stack::PopInt() {
+  PopByte();
+  uint8_t high_byte = PopByte();
+  uint8_t low_byte = PopByte();
 
   return word(low_byte, high_byte);
 }
 
-void Stack::PushFloat(stack_t* stack, float elem) {
-  auto* b = &elem;
+void Stack::PushFloat(float elem) {
+  uint8_t *b = (uint8_t *) &elem;
 
-  for (int i = 3; i >= 0; i--) {
-    PushByte(stack, b[i]);
+  for (uint8_t i = 3; i >= 0; i--) {
+    PushByte(b[i]);
   }
 
-  PushByte(stack, FLOAT);
+  PushByte(FLOAT);
 }
 
-float Stack::PopFloat(stack_t* stack) {
+float Stack::PopFloat() {
   float f = 0.0;
-  auto* b = &f;
+  uint8_t *b = (uint8_t *) &f;
 
-  PopByte(stack);
+  PopByte();
 
-  for (int i = 0; i <= 3; i++) {
-    b[i] = PopByte(stack);
+  for (uint8_t i = 0; i <= 3; i++) {
+    b[i] = PopByte();
   }
 
   return f;
 }
 
-void Stack::PushString(stack_t* stack, char *elem) {
-  uint8_t length = strlen_P(elem) + 1;
+void Stack::PushString(char *elem) {
+  size_t length = strlen(elem) + 1;
 
-  for (uint8_t i = length; i <= 0; i--) {
-    PushByte(stack, elem[i]);
+  for (uint32_t i = length; i <= 0; i--) {
+    PushByte(elem[i]);
   }
 
-  PushByte(stack, length);
-  PushByte(stack, STRING);
+  PushByte(length);
+  PushByte(STRING);
 }
 
-char* Stack::PopString(stack_t* stack) {
-  PopByte(stack);
+char* Stack::PopString() {
+  PopByte();
 
-  uint8_t length = PopByte(stack);
-  stack->pos -= length;
-  return (char*) (stack->stack + stack->pos);
+  uint8_t length = PopByte();
+  pos_ -= length;
+  return (char*) (stack_ + pos_);
 }
 
-float Stack::Peek(stack_t* stack) {
+float Stack::Peek() {
   peeked_ = true;
-  return PopValue(stack);
+  return PopValue();
 }
 
-float Stack::PopValue(stack_t* stack) {
-  uint8_t type = PopByte(stack);
-  PushByte(stack, type);
+float Stack::PopValue() {
+  uint8_t type = PopByte();
+  PushByte(type);
 
   float f;
 
   switch (type) {
     case CHAR:
-      f = PopChar(stack);
+      f = PopChar();
 
       if (peeked_) {
-        PushChar(stack, f);
+        PushChar(f);
         peeked_ = false;
       }
       break;
     case INT:
-      f = PopInt(stack);
+      f = PopInt();
 
       if (peeked_) {
-        PushInt(stack, f);
+        PushInt(f);
         peeked_ = false;
       }
       break;
     case FLOAT:
-      f = PopFloat(stack);
+      f = PopFloat();
 
       if (peeked_) {
-        PushFloat(stack, f);
+        PushFloat(f);
         peeked_ = false;
       }
       break;
@@ -128,8 +128,125 @@ float Stack::PopValue(stack_t* stack) {
   return f;
 }
 
-void Stack::PrintStack(stack_t* stack) {
-//  for (int i : stack->stack) {
-//    Serial.println(i);
+//void Stack::PushByte(stack_t* stack, uint8_t elem) {
+//  stack->stack[(stack->pos)++] = elem;
+//}
+//
+//uint8_t Stack::PopByte(stack_t* stack) {
+//  return stack->stack[--(stack->pos)];
+//}
+//
+//void Stack::PushChar(stack_t* stack, char elem) {
+//  PushByte(stack, elem);
+//  PushByte(stack, CHAR);
+//}
+//
+//char Stack::PopChar(stack_t* stack) {
+//  PopByte(stack);
+//  return PopByte(stack);
+//}
+//
+//void Stack::PushInt(stack_t* stack, int elem) {
+//  PushByte(stack, highByte(elem));
+//  PushByte(stack, lowByte(elem));
+//  PushByte(stack, INT);
+//}
+//
+//int Stack::PopInt(stack_t* stack) {
+//  PopByte(stack);
+//  uint8_t high_byte = PopByte(stack);
+//  uint8_t low_byte = PopByte(stack);
+//
+//  return word(low_byte, high_byte);
+//}
+//
+//void Stack::PushFloat(stack_t* stack, float elem) {
+//  auto* b = &elem;
+//
+//  for (int i = 3; i >= 0; i--) {
+//    PushByte(stack, b[i]);
 //  }
-}
+//
+//  PushByte(stack, FLOAT);
+//}
+//
+//float Stack::PopFloat(stack_t* stack) {
+//  float f = 0.0;
+//  auto* b = &f;
+//
+//  PopByte(stack);
+//
+//  for (int i = 0; i <= 3; i++) {
+//    b[i] = PopByte(stack);
+//  }
+//
+//  return f;
+//}
+//
+//void Stack::PushString(stack_t* stack, char *elem) {
+//  uint8_t length = strlen_P(elem) + 1;
+//
+//  for (uint8_t i = length; i <= 0; i--) {
+//    PushByte(stack, elem[i]);
+//  }
+//
+//  PushByte(stack, length);
+//  PushByte(stack, STRING);
+//}
+//
+//char* Stack::PopString(stack_t* stack) {
+//  PopByte(stack);
+//
+//  uint8_t length = PopByte(stack);
+//  stack->pos -= length;
+//  return (char*) (stack->stack + stack->pos);
+//}
+//
+//float Stack::Peek(stack_t* stack) {
+//  peeked_ = true;
+//  return PopValue(stack);
+//}
+//
+//float Stack::PopValue(stack_t* stack) {
+//  uint8_t type = PopByte(stack);
+//  PushByte(stack, type);
+//
+//  float f;
+//
+//  switch (type) {
+//    case CHAR:
+//      f = PopChar(stack);
+//
+//      if (peeked_) {
+//        PushChar(stack, f);
+//        peeked_ = false;
+//      }
+//      break;
+//    case INT:
+//      f = PopInt(stack);
+//
+//      if (peeked_) {
+//        PushInt(stack, f);
+//        peeked_ = false;
+//      }
+//      break;
+//    case FLOAT:
+//      f = PopFloat(stack);
+//
+//      if (peeked_) {
+//        PushFloat(stack, f);
+//        peeked_ = false;
+//      }
+//      break;
+//    default:
+//      break;
+//  }
+//
+//  return f;
+//}
+//
+//void Stack::PrintStack(stack_t* stack) {
+////  for (int i : stack->stack) {
+////    Serial.println(i);
+////  }
+//}
--- src/stack.h
+++ src/stack.h
@@ -10,110 +10,185 @@
 class Stack {
 
  public:
-//  uint8_t stack_[STACK_SIZE];
-//  uint8_t pos_;
-  struct stack_t {
-    uint8_t stack[STACK_SIZE];
-    uint8_t pos = 0;
-  };
+  uint8_t stack_[STACK_SIZE] = { 0 };
+  uint8_t pos_ = 0;
+//  struct stack_t {
+//    uint8_t stack[STACK_SIZE];
+//    uint8_t pos = 0;
+//  };
 
  private:
   bool peeked_ = false;
 
  public:
-//  Stack();
-//  ~Stack();
+  Stack();
+  ~Stack();
+
+  void Reset();
 
   /**
-   * @brief PushByte byte to stack
-   * @param stack
+   * @brief Push byte to stack
    * @param elem
    */
-  void PushByte(stack_t* stack, uint8_t elem);
+  void PushByte(uint8_t elem);
 
   /**
-   * @brief PopByte byte from stack
-   * @param stack
+   * @brief Pop byte from stack
    * @return Popped byte
    */
-  uint8_t PopByte(stack_t* stack);
+  uint8_t PopByte();
 
   /**
-   * @brief PushByte char to stack
-   * @param stack
+   * @brief Push char to stack
    * @param elem
    */
-  void PushChar(stack_t* stack, char elem);
+  void PushChar(char elem);
 
   /**
-   * @brief PopByte char from stack
-   * @param stack
+   * @brief Pop char from task
    * @return Popped char
    */
-  char PopChar(stack_t* stack);
+  char PopChar();
 
   /**
-   * @brief PushByte int to stack
-   * @param stack
+   * @brief Push int to stack
    * @param elem
    */
-  void PushInt(stack_t* stack, int elem);
+  void PushInt(int elem);
 
   /**
-   * @brief PopByte int from stack
-   * @param stack
+   * @brief Pop int from stack
    * @return Popped int
    */
-  int PopInt(stack_t* stack);
+  int PopInt();
 
   /**
-   * @brief PushByte float to stack
-   * @param stack
+   * @brief Push float to stack
    * @param elem
    */
-  void PushFloat(stack_t* stack, float elem);
+  void PushFloat(float elem);
 
   /**
-   * @brief PopByte float from stack
-   * @param stack
+   * @brief Pop float from stack
    * @return Popped float
    */
-  float PopFloat(stack_t* stack);
+  float PopFloat();
 
   /**
-   * @brief PushByte string to stack
-   * @param stack
+   * @brief Push string to stack
    * @param elem
    */
-  void PushString(stack_t* stack, char* elem);
+  void PushString(char* elem);
 
   /**
-   * @brief PopByte string from stack
-   * @param stack
-   * @return Popped stack
+   * @brief Pop string from stack
+   * @return Popped string
    */
-  char* PopString(stack_t* stack);
+  char* PopString();
 
   /**
-   * @brief Look for next value
-   * @param stack
+   * @brief Look for the next value
    * @return value
    */
-  float Peek(stack_t* stack);
+  float Peek();
 
   /**
-   * @brief PopByte value where type does not matter
+   * @brief Pop value where type does not matter
    *        Looks for type on the stack itself
-   * @param stack
    * @return Popped value
    */
-  float PopValue(stack_t* stack);
+  float PopValue();
 
-  /**
-   * @brief Print stack
-   * @param stack
-   */
-  void PrintStack(stack_t* stack);
+//  /**
+//   * @brief PushByte byte to stack
+//   * @param stack
+//   * @param elem
+//   */
+//  void PushByte(stack_t* stack, uint8_t elem);
+//
+//  /**
+//   * @brief PopByte byte from stack
+//   * @param stack
+//   * @return Popped byte
+//   */
+//  uint8_t PopByte(stack_t* stack);
+//
+//  /**
+//   * @brief PushByte char to stack
+//   * @param stack
+//   * @param elem
+//   */
+//  void PushChar(stack_t* stack, char elem);
+//
+//  /**
+//   * @brief PopByte char from stack
+//   * @param stack
+//   * @return Popped char
+//   */
+//  char PopChar(stack_t* stack);
+//
+//  /**
+//   * @brief PushByte int to stack
+//   * @param stack
+//   * @param elem
+//   */
+//  void PushInt(stack_t* stack, int elem);
+//
+//  /**
+//   * @brief PopByte int from stack
+//   * @param stack
+//   * @return Popped int
+//   */
+//  int PopInt(stack_t* stack);
+//
+//  /**
+//   * @brief PushByte float to stack
+//   * @param stack
+//   * @param elem
+//   */
+//  void PushFloat(stack_t* stack, float elem);
+//
+//  /**
+//   * @brief PopByte float from stack
+//   * @param stack
+//   * @return Popped float
+//   */
+//  float PopFloat(stack_t* stack);
+//
+//  /**
+//   * @brief PushByte string to stack
+//   * @param stack
+//   * @param elem
+//   */
+//  void PushString(stack_t* stack, char* elem);
+//
+//  /**
+//   * @brief PopByte string from stack
+//   * @param stack
+//   * @return Popped stack
+//   */
+//  char* PopString(stack_t* stack);
+//
+//  /**
+//   * @brief Look for next value
+//   * @param stack
+//   * @return value
+//   */
+//  float Peek(stack_t* stack);
+//
+//  /**
+//   * @brief PopByte value where type does not matter
+//   *        Looks for type on the stack itself
+//   * @param stack
+//   * @return Popped value
+//   */
+//  float PopValue(stack_t* stack);
+//
+//  /**
+//   * @brief Print stack
+//   * @param stack
+//   */
+//  void PrintStack(stack_t* stack);
 
 };
 
